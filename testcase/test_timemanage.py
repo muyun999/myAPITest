5bc4020d8b67cf715f403bef4736f0443c86fd62
jinxy <752074370@qq.com>
2023-03-20 16:09:33 +0800

    代码重构

---
+++ testcase/test_timemanage.py
@@ -0,0 +1,13 @@
+import pytest
+from common.read_config import ReadConfig
+from common.request_builder import get_all_case_datas
+from common.request_tool import CommonHttp
+from common.request_builder import run_case_data
+localReadConfig = ReadConfig()
+CommonHttp = CommonHttp()
+
+
+class Testtimemanage():
+    @pytest.mark.parametrize('case_data', get_all_case_datas("timemanage"))
+    def test_line(self, case_data):
+        run_case_data(case_data)
